cf6599ef75dcf93d2a55599cc11f84e6e1ca397b
Juanpablo <juanpablopj20003@gmail.com>
2021-07-09 22:31:54 -0400

    lector de csv

--- resolucion_proyecto_9.py
+++ resolucion_proyecto_9.py
@@ -1,1 +1,6 @@
-## testeo de git
+import csv
+with open('ResidenciasSanitarias.csv', newline='') as csvbdd:
+    lectordatos = csv.reader(csvbdd,  delimiter=' ', quotechar='|' )
+    for i in lectordatos:
+        print(', '.join(i))
+
