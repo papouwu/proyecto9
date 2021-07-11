94bb7366147ec7492bf2602838912f9bdfc841a0
papouwu <87214376+papouwu@users.noreply.github.com>
2021-07-11 14:28:12 -0400

    pusimos otras 2 funciones, una tiene el lick de las residencias sanitarias y la otro funcion hace que el individuo introdusca la fecha que busca, tambien creamos un diccionario

--- resolucion_proyecto_9.py
+++ resolucion_proyecto_9.py
@@ -51,6 +51,39 @@ def continuar ():
 
 
 
+def recibirFecha():
+    fecha = ""
+    fecha = input("Ingrese la fecha a buscar en forma DD-MM-AAAA(solo digitos separados por guiones):")
+    return fecha
+
+
+
+
+def leerArchivo():
+    c = pd.read_csv("https://raw.githubusercontent.com/MinCiencia/Datos-COVID19/master/output/producto36/ResidenciasSanitarias.csv")
+    print(c)
+
+
+regiones ={ "1"  :"Arica y Parinacota",
+            "2"  :"Tarapacá",
+            "3"  :"Antofagasta",
+            "4"  :"Atacama",
+            "5"  :"Coquimbo",
+            "6"  :"Valparaíso",
+            "7"  :"Metropolitana",
+            "8"  :"O'Higgins",
+            "9"  :"Maule",
+            "10" :"Ñuble",
+            "11" :"Biobío",
+            "12" :"Araucanía",
+            "13" :"Los Ríos",
+            "14" :"Los Lagos",
+            "15" : "Aysén",
+            "16" :"Magallanes"}
+
+
+
+
 
 
 
@@ -62,24 +95,24 @@ def continuar ():
                         
 with open('ResidenciasSanitarias.csv', newline='') as csvbdd:
     lectordatos = csv.reader(csvbdd,  delimiter=' ', quotechar='|' )
-    for i in lectordatos:''
+    for i in lectordatos:'''
     
 
 while True: 
     print("bienvenido al programa de estadisitica de ocupacion de Residencias Sanitarias. Para continuar, selecione una opcion:")
     print('''
-   ''' opcion 1: Seleccione una región para mostrar los datos respecto a ocupación de Residencias Sanitarias de los últimos 7 días.
+    opcion 1: Seleccione una región para mostrar los datos respecto a ocupación de Residencias Sanitarias de los últimos 7 días.
     opcion 2: Mostrar la región con mayor cantidad de residencias, mayor cantidad de cupos para esas residencias y mayor cantidad de residentes.
     opcion 3: Mostrar la región con menor cantidad de residencias, menor cantidad de cupos para esas residencias y menor cantidad de residentes.
     opcion 4: Bucar por fecha y región para mostrar residentes vs. cupos.
-    opcion 5: Mostrar densidad de ocupación de las residencias para las últimas 7 fechas a nivel nacional.''')'''
+    opcion 5: Mostrar densidad de ocupación de las residencias para las últimas 7 fechas a nivel nacional.''')
     
-   ''' opcion=int(input("ingrese su opcion:"))
+    opcion=int(input("ingrese su opcion:"))
     if opcion == 1:
         regiones=[]
         sigla=input("ingrese el numero de la region deseada:")
     
-    elif opcion == 2:
+    '''elif opcion == 2:
 
     elif opcion == 3:
 
